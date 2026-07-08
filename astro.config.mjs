06c1e6e3ce89a20117bba7372a84ef86d1abfca6
Payziii <velmikhail123@gmail.com>
2026-07-08 09:52:25 +0500

    [ / ] Changed config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,32 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'PayziBot',
+			defaultLocale: 'ru',
+			locales: {
+				en: {
+					label: 'English',
+					lang: 'en',
+				},
+				ru: {
+					label: 'Русский',
+					lang: 'ru',
+				},
+			},
+			social: [
+				{ icon: 'discord', label: 'Add bot', href: 'https://discord.com/api/oauth2/authorize?client_id=576442351426207744&permissions=1411299798102&scope=bot' },
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/PayziBot/PayziBotDocs' }
+			],
+			editLink: {
+				baseUrl: 'https://github.com/PayziBot/PayziBotDocs/edit/main/'
+			},
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
-						// Each item here is one entry in the navigation menu.
 						{ label: 'Example Guide', slug: 'guides/example' },
 					],
 				},
