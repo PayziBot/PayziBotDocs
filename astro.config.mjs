944b3ad5a07491a0e29bf3d251bebf210b348ac3
Payziii <velmikhail123@gmail.com>
2026-07-12 16:52:43 +0500

    [ + ] Main page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,44 +1,51 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import Icons from 'starlight-plugin-icons'
+import UnoCSS from 'unocss/astro'
 
 export default defineConfig({
 	redirects: {
 		'/': '/ru',
 	},
 	integrations: [
-		starlight({
-			title: 'PayziBot',
-			defaultLocale: 'ru',
-			locales: {
-				en: {
-					label: 'English',
-					lang: 'en',
+		UnoCSS(),
+		Icons({
+			codeblock: true,
+			extractSafelist: true,
+			starlight: {
+				title: 'PayziBot',
+				defaultLocale: 'ru',
+				locales: {
+					en: {
+						label: 'English',
+						lang: 'en',
+					},
+					ru: {
+						label: 'Русский',
+						lang: 'ru',
+					},
 				},
-				ru: {
-					label: 'Русский',
-					lang: 'ru',
+				social: [
+					{ icon: 'discord', label: 'Add bot', href: 'https://discord.com/api/oauth2/authorize?client_id=576442351426207744&permissions=1411299798102&scope=bot' },
+					{ icon: 'github', label: 'GitHub', href: 'https://github.com/PayziBot/PayziBotDocs' }
+				],
+				editLink: {
+					baseUrl: 'https://github.com/PayziBot/PayziBotDocs/edit/main/'
 				},
-			},
-			social: [
-				{ icon: 'discord', label: 'Add bot', href: 'https://discord.com/api/oauth2/authorize?client_id=576442351426207744&permissions=1411299798102&scope=bot' },
-				{ icon: 'github', label: 'GitHub', href: 'https://github.com/PayziBot/PayziBotDocs' }
-			],
-			editLink: {
-				baseUrl: 'https://github.com/PayziBot/PayziBotDocs/edit/main/'
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
+				sidebar: [
+					{
+						label: 'Guides',
+						items: [
+							{ label: 'Example Guide', slug: 'guides/example' },
+						],
+					},
+					{
+						label: 'Reference',
+						// @ts-expect-error — несовпадение типов между starlight-plugin-icons и @astrojs/starlight
+						items: [{ autogenerate: { directory: 'reference' } }],
+					},
+				],
+			}
 		}),
 	],
 });
